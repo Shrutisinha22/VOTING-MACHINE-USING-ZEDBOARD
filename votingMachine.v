35dea47eb6122e3891419acbf204ad002bf6c9b6
Shrutisinha22 <86293764+Shrutisinha22@users.noreply.github.com>
2021-06-24 23:26:46 +0530

    uploaded source files

---
+++ votingMachine.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2021 12:34:00 AM
+// Design Name: 
+// Module Name: votingMachine
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module votingMachine(
+
+    );
+endmodule
